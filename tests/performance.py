7e2428ee18035f0cd860acd353b276207a2ec2d6
MariuszMJ <mariusz.michna.j@gmail.com>
2023-03-31 15:24:55 +0200

    Test Execution Time and Memory comsumption

--- tests/performance.py
+++ tests/performance.py
@@ -1,53 +1,43 @@
-import timeit
-from count_islands import count_islands_bfs, count_islands_dfs
+import pytest
 import random
+import timeit
+import tracemalloc
+from count_islands import count_islands_bfs, count_islands_dfs
 
 
-def case_small(count_islands_func):
-    matrix = [
-        [0, 1, 0],
-        [0, 0, 0],
-        [0, 1, 1],
-    ]
-    return count_islands_func(matrix)
-
-
-def case_medium(count_islands_func):
-    matrix = [
-        [0, 1, 0, 0, 0, 1, 0, 0, 1, 1],
-        [0, 0, 0, 1, 1, 0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 1, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-        [1, 1, 0, 1, 1, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-        [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-        [0, 1, 0, 1, 1, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 1, 0, 1, 1],
-        [1, 1, 1, 1, 0, 0, 0, 1, 0, 0],
-    ]
-    return count_islands_func(matrix)
-
-
-def case_large(count_islands_func):
+def generate_random_matrix(rows, cols):
     random.seed(42)
-    matrix = [[random.choice([0, 1]) for _ in range(100)] for _ in range(100)]
+    return [[random.choice([0, 1]) for _ in range(cols)] for _ in range(rows)]
 
-    return count_islands_func(matrix)
 
-print("Execution Time Comparison")
+test_data = [
+    ("small", generate_random_matrix(3, 3)),
+    ("medium", generate_random_matrix(40, 40)),
+    ("large", generate_random_matrix(1000, 1000)),
+]
 
-algorithms = [("BFS", count_islands_bfs), ("DFS", count_islands_dfs)]
 
-for algo_name, algo_func in algorithms:
-    print(
-        f"{algo_name} small test case:",
-        timeit.timeit(lambda: case_small(algo_func), number=1000),
-    )
-    print(
-        f"{algo_name} medium test case:",
-        timeit.timeit(lambda: case_medium(algo_func), number=1000),
-    )
-    print(
-        f"{algo_name} large test case:",
-        timeit.timeit(lambda: case_large(algo_func), number=1000),
-    )
+@pytest.mark.parametrize("algo_name, algo_func", [("BFS", count_islands_bfs), ("DFS", count_islands_dfs)])
+@pytest.mark.parametrize("size, matrix", test_data)
+def test_time_execution(algo_name, algo_func, size, matrix, num_runs=1000):
+    start_time = timeit.default_timer()
+    for _ in range(num_runs):
+        algo_func(matrix)
+    elapsed_time = timeit.default_timer() - start_time
+    avg_time = elapsed_time / num_runs
+    print(f"{algo_name} {size} test case ({num_runs} runs): {avg_time:.6f} seconds per run")
+
+
+def measure_memory(func, matrix):
+    tracemalloc.start()
+    func(matrix)
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    return peak / 1024
+
+
+@pytest.mark.parametrize("algo_name, algo_func", [("BFS", count_islands_bfs), ("DFS", count_islands_dfs)])
+@pytest.mark.parametrize("size, matrix", test_data)
+def test_memory_consumption(algo_name, algo_func, size, matrix):
+    memory = measure_memory(algo_func, matrix)
+    print(f"Memory usage {algo_name} {size} test case: {memory:.6f} KB")
