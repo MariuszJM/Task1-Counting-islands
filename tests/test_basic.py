1053f230f1f18a299dbbc031c1816cd94613cd88
MariuszMJ <mariusz.michna.j@gmail.com>
2023-03-31 12:04:14 +0200

    Add deepcopy to protect from modifying test cases

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,9 @@
 import pytest
-from count_islands import count_islands_dfs
+from count_islands import count_islands_dfs, count_islands_bfs
+import copy
 
 
-@pytest.mark.parametrize("count_islands_func", [count_islands_dfs])
+@pytest.mark.parametrize("count_islands_func", [count_islands_dfs, count_islands_bfs])
 def test_basic(count_islands_func, basic_test_case):
-    input_matrix, expected_output = basic_test_case
+    input_matrix, expected_output = copy.deepcopy(basic_test_case)
     assert count_islands_func(input_matrix) == expected_output
