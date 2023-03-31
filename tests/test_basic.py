f2f9691516a5490241aeb08bb109b9b7570f11be
MariuszMJ <mariusz.michna.j@gmail.com>
2023-03-31 11:43:01 +0200

    Function rename

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,8 @@
 import pytest
-from count_islands import count_islands_greedy
+from count_islands import count_islands_dfs
 
 
-@pytest.mark.parametrize("count_islands_func", [count_islands_greedy])
+@pytest.mark.parametrize("count_islands_func", [count_islands_dfs])
 def test_basic(count_islands_func, basic_test_case):
     input_matrix, expected_output = basic_test_case
     assert count_islands_func(input_matrix) == expected_output
